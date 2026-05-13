d8bd2a75a9f4204c7e1473d5af932c307fa6860d
Samip Pokharel <144752715+samipsamip@users.noreply.github.com>
2026-05-13 22:19:14 +0100

    Move constants to its own file

---
+++ src/api/src/utils/constants.ts
@@ -0,0 +1,3 @@
+export const EMAIL_TEMPLATE_ID = {
+	welcomeEmail: '179449ee-8e31-4116-9535-be7f1eca7245',
+} as const; // better tree shaking and not widening the type to string
--- src/api/src/utils/email_utils.ts
+++ src/api/src/utils/email_utils.ts
@@ -1,37 +1,34 @@
-import { Resend } from "resend";
-
-const EMAIL_TEMPLATE_ID = {
-  welcomeEmail: "179449ee-8e31-4116-9535-be7f1eca7245",
-} as const; // better tree shaking and not widening the type to string
+import { Resend } from 'resend';
+import { EMAIL_TEMPLATE_ID } from './constants';
 
 export default class ProductEmail {
-  constructor(private emailClient: Resend) {}
-  async sendVerificationEmail(
-    fullName: string,
-    email: string,
-    url: string,
-  ): Promise<void> {
-    console.log(fullName, email, url);
-    const { data, error } = await this.emailClient.emails.send({
-      to: email,
-      template: {
-        id: EMAIL_TEMPLATE_ID.welcomeEmail,
-        variables: {
-          fullName: fullName,
-          verificationURL: url,
-        },
-      },
-    });
-    console.log(error);
-    //TO DO : Add error handling and logging
-  }
-  async sendResetPasswordEmail(
-    _fullName: string,
-    _email: string,
-    _url: string,
-  ): Promise<void> {}
+	constructor(private emailClient: Resend) {}
+	async sendVerificationEmail(
+		fullName: string,
+		email: string,
+		url: string,
+	): Promise<void> {
+		console.log(fullName, email, url);
+		const { data, error } = await this.emailClient.emails.send({
+			to: email,
+			template: {
+				id: EMAIL_TEMPLATE_ID.welcomeEmail,
+				variables: {
+					fullName: fullName,
+					verificationURL: url,
+				},
+			},
+		});
+		console.log(error);
+		//TO DO : Add error handling and logging
+	}
+	async sendResetPasswordEmail(
+		_fullName: string,
+		_email: string,
+		_url: string,
+	): Promise<void> {}
 }
 
 export const emailSender = new ProductEmail(
-  new Resend(process.env.RESEND_API_KEY ?? ""),
+	new Resend(process.env.RESEND_API_KEY ?? ''),
 );
