c0ea4fecd1238b5326f0af111e1b2dda568e59e2
Samip Pokharel <144752715+samipsamip@users.noreply.github.com>
2026-05-13 22:18:11 +0100

    Ignore lint on shadcn components

--- src/ui/src/main.tsx
+++ src/ui/src/main.tsx
@@ -1,14 +1,15 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router";
-import { router } from "./lib/routes.tsx";
-import "./App.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "sonner";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router';
+import { router } from './lib/routes.tsx';
+import './App.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from 'sonner';
 
 const _queryClient = new QueryClient();
 
-createRoot(document.getElementById("root")!).render(
+// biome-ignore lint/style/noNonNullAssertion: Root is always going to be present
+createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<QueryClientProvider client={_queryClient}>
 			<RouterProvider router={router} />
