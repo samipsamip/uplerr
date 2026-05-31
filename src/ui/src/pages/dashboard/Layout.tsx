80385f8769d8b3af0a9377df75df1cf02ccc7c74
Samip Pokharel <144752715+samipsamip@users.noreply.github.com>
2026-05-31 03:18:30 +0100

    Default sidebar to closed

--- src/ui/src/pages/dashboard/Layout.tsx
+++ src/ui/src/pages/dashboard/Layout.tsx
@@ -5,7 +5,7 @@ import { TooltipProvider } from '@/components/ui/tooltip';
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<TooltipProvider>
-			<SidebarProvider className="h-svh overflow-hidden">
+			<SidebarProvider className="h-svh overflow-hidden" defaultOpen={false}>
 				<AppSidebar variant="inset" />
 				<main className="bg-muted flex flex-1 flex-col overflow-auto">
 					{children}
